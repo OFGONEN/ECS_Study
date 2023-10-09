7ab71310bea2e0f23541b21bd8b0ac23b5d75f7b
ofgonen-adastec <ofgonen@gmail.com>
2023-10-09 17:34:28 +0300

    feat: refactor spawner system using Jobs

--- Assets/SpawnerTutorial/Systems/SpawnerSystem.cs
+++ Assets/SpawnerTutorial/Systems/SpawnerSystem.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -14,21 +15,51 @@ namespace Tutorial.Spawner
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            foreach (var spawner in SystemAPI.Query<RefRW<Spawner>>())
+            var ecb_parallel = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
+
+
+            var processSpawnerJobHandle = new ProcessSpawnerJob
             {
-                ProcessSpawner(ref state, spawner);
-            }
+                ECB = ecb_parallel,
+                ElapsedTime = SystemAPI.Time.ElapsedTime,
+            }.ScheduleParallel(state.Dependency);
+
+            state.Dependency = processSpawnerJobHandle;
+
+            // foreach (var spawner in SystemAPI.Query<RefRW<Spawner>>())
+            // {
+            //     ProcessSpawner(ref state, spawner);
+            // }
         }
 
-        private void ProcessSpawner(ref SystemState state, RefRW<Spawner> spawner)
-        {
-            if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
-            {
-                Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
-                
-                state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition));
+        // private void ProcessSpawner(ref SystemState state, RefRW<Spawner> spawner)
+        // {
+        //     if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
+        //     {
+        //         Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
+        //         
+        //         state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition));
+        //
+        //         spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+        //     }
+        // }
+    }
+    
+    [BurstCompile]
+    public partial struct ProcessSpawnerJob : IJobEntity
+    {
+        public EntityCommandBuffer.ParallelWriter ECB;
+        public double ElapsedTime;
 
-                spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+        private void Execute([ChunkIndexInQuery] int chunkIndex, ref Spawner spawner)
+        {
+            if (spawner.NextSpawnTime < ElapsedTime)
+            {
+                Entity newEntity = ECB.Instantiate(chunkIndex, spawner.Prefab);
+                ECB.SetComponent(chunkIndex, newEntity, LocalTransform.FromPosition(spawner.SpawnPosition));
+
+                spawner.NextSpawnTime = (float)ElapsedTime + spawner.SpawnRate;
             }
         }
     }
